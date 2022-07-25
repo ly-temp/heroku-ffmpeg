c4c3d5203e8868e15635d3debe7da79c8b0edefd
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-07-25 17:57:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 <form action="upload.php" method="post" enctype="multipart/form-data">
-  <input name="upload[]" type="file" multiple="multiple" />
+  <input name="upload[]" type="file" multiple />
   <input type="submit" value="Send files" />
 </form>
 ?>
