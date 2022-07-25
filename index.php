5d1c4e7b6285e84a9391d4577ad98d9c4a19b63f
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-07-25 17:55:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-
-echo "HI"
-  
+<form action="upload.php" method="post" enctype="multipart/form-data">
+  Send these files:<br />
+  <input name="userfile[]" type="file" /><br />
+  <input type="submit" value="Send files" />
+</form>
 ?>
