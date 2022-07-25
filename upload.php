1edf94ecf70f60db2ce5d2673e2897dc8bcf72b4
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-07-25 19:00:49 +0800

    Update upload.php

--- upload.php
+++ upload.php
@@ -20,6 +20,7 @@ for( $i=0 ; $i < $total ; $i++ ) {
     move_uploaded_file($tmpFilePath, $newFilePath);
   }
 }
-echo shell_exec("ls ".$target_dir);
+output = shell_exec("ls ".$target_dir);
+echo str_replace("\n","<br>",$output);
 
 ?>
