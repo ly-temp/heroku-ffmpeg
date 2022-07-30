0c512c887a1acf6cb9c11672d45da254c77a45d9
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-07-30 18:32:57 +0800

    Update list_file.php

--- list_file.php
+++ list_file.php
@@ -5,7 +5,13 @@ $sep = "~~~~~~~~~";
 echo $sep."uploads".$sep."<br>";
 $output = shell_exec("ls ".$target_dir);
 $output = explode("\n", $output);
-echo '<form action="/ffmpeg.php"  method="post">';
+
+exec("mkdir -p ".$out_dir);
+echo $sep."output".$sep."<br>";
+$output = shell_exec("ls ".$out_dir);
+echo str_replace("\n","<br>",$output);
+
+function print_options($output){
 /**mode
 //0: no action
 //1: nokia
@@ -18,20 +24,18 @@ echo '<form action="/ffmpeg.php"  method="post">';
 //post parameter:
 //$i->option
 //c$i->command/value
+echo '<form action="/ffmpeg.php"  method="post">';
 for($i = 0; $i < count($output); $i++){
+    echo $output[$i].'<select name="'.$i.'">
+    <option value="0">no action</option>
+    <option value="1">nokia</option>
+    <option value="2">audio to db</option>
+    <option value="3">change format</option>
+    <option value="-1">command</option>
+    <input type="text" name="c'.$i.'">
+    </select><br>';
+  }
+  echo '<input type="submit"></form>';
 
-  echo '<select name="'.$i.'">
-  <option value="0">no action</option>
-  <option value="1">nokia</option>
-  <option value="2">audio to db</option>
-  <option value="3">change format</option>
-  <option value="-1">command</option>
-  <input type="text" name="c'.$i.'">
-  </select><br>';
 }
-echo '<input type="submit"></form>';
-exec("mkdir -p ".$out_dir);
-echo $sep."output".$sep."<br>";
-$output = shell_exec("ls ".$out_dir);
-echo str_replace("\n","<br>",$output);
 ?>
