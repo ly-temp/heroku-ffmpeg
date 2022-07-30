2f23bb5a13325663a8bf3f1b333603f5ab931ac3
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-07-30 19:11:41 +0800

    Update ffmpeg.php

--- ffmpeg.php
+++ ffmpeg.php
@@ -1,5 +1,22 @@
 <?php
+ 
   for($i = 0; !empty($_POST[$i]); $i++){
     echo $i."->".$_POST['c'.$i].'<br>';
+    switch($_POST[$i]){
+      //case 1://no action
+        //break;
+      case 2://nokia
+        break;
+      case 3://audio to db
+        break;
+      case 4://change format
+        break;
+      case -1:
+        break;
+    }
+  }
+
+  function has_video($file){
+    return shell_exec("ffprobe -v error -select_streams v:0 -show_entries stream=codec_type -of csv=p=0 ".$file) === "";
   }
 ?>
