037d2609b3892fedcd7ac839ecc8637eca307edd
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-07-30 18:13:06 +0800

    Update list_file.php

--- list_file.php
+++ list_file.php
@@ -4,7 +4,14 @@ $out_dir = "./output";
 $sep = "~~~~~~~~~";
 echo $sep."uploads".$sep."<br>";
 $output = shell_exec("ls ".$target_dir);
-echo str_replace("\n","<br>",$output);
+$output = explode("\n", $output);
+echo '<form action="/ffmpeg.php"  method="post">';
+for($i = 0; $i < count($output); $i++){
+  echo '<input type="radio" name=".$i." value="radio">Fit Description<br>';
+}
+echo "</form>"
 exec("mkdir -p ".$out_dir);
 echo $sep."output".$sep."<br>";
+$output = shell_exec("ls ".$out_dir);
+echo str_replace("\n","<br>",$output);
 ?>
