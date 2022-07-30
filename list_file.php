0c72fbebab1606fa75ed7a0f7b31016bf7261eb7
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-07-30 17:50:07 +0800

    Update list_file.php

--- list_file.php
+++ list_file.php
@@ -1,9 +1,8 @@
 <?php
 $target_dir = "./uploads";
-$out_dir = "./output";
 
 $output = shell_exec("ls ".$target_dir);
 echo str_replace("\n","<br>",$output);
-exec("mkdir -p ".$out_dir);
-echo shell_exec("ls");
+
+
 ?>
