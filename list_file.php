ecaba2edb22157cf5cf3cc2c979a7f129c05a8ea
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-07-30 19:13:46 +0800

    Update list_file.php

--- list_file.php
+++ list_file.php
@@ -4,12 +4,12 @@ $out_dir = "./output";
 $sep = "~~~~~~~~~";
 echo $sep."uploads".$sep."<br>";
 $output = shell_exec("ls ".$target_dir." | tee uploads.list");
-handle_ls($output);
+handle_ls("u", $output);
 
 exec("mkdir -p ".$out_dir);
 echo $sep."output".$sep."<br>";
 $output = shell_exec("ls ".$out_dir." | tee output.list");
-handle_ls($output);
+handle_ls("o", $output);
 
 
 //input is pure string
@@ -19,7 +19,9 @@ function handle_ls($output){
     print_options($explode);
 }
 
-function print_options($output){
+function print_options($prefix, $output){
+//prefix: u: upload folder
+//        o: output folder
 /**mode
 //1: no action
 //2: nokia
@@ -34,7 +36,7 @@ function print_options($output){
 //c$i->command/value
 echo '<form action="/ffmpeg.php"  method="post">';
 for($i = 0; $i < count($output); $i++){
-    echo $output[$i].'<select name="'.$i.'">
+    echo $output[$i].'<select name="'.$prefix.$i.'">
     <option value="1">no action</option>
     <option value="2">nokia</option>
     <option value="3">audio to db</option>
