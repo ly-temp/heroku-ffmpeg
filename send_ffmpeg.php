ec259fc3faae3f657d62eb50285ef54a428a79a6
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-07-31 12:37:29 +0800

    Update send_ffmpeg.php

--- send_ffmpeg.php
+++ send_ffmpeg.php
@@ -12,6 +12,7 @@ exec("mkdir -p ".$out_dir);
 echo $sep."output".$sep."<br>";
 $output = shell_exec("ls ".$out_dir." | tee output.list");
 handle_ls("o", $output);
+echo  'timeout:<input type="text" name=timeout>';
 echo '<button type="submit">submit</button></form>';
 
 //input is pure string
