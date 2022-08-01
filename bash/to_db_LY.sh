3504068ed3ae599d3af680b53678029ee9e9e0f1
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-01 17:30:26 +0800

    Update to_db_LY.sh

--- bash/to_db_LY.sh
+++ bash/to_db_LY.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#input: [output suffix] [input file] [volume]
-target_db=$3
-if [ "$1" = "" ]; then
-	filename=$(basename -- "$2")
+#input: [input file] [volume] [output suffix]
+target_db=$2
+if [ "$3" = "" ]; then
+	filename=$(basename -- "$1")
 	suffix=".${filename##*.}"
 else
-	suffix=$1
+	suffix=$3
 fi
 
 diff_db(){
@@ -24,8 +24,8 @@ done
 echo $diff
 }
 
-echo "in file: $2"
-file=$2
+echo "in file: $1"
+file=$1
 diff=$(diff_db)
 value=0
 str_value=""
@@ -37,13 +37,14 @@ while
 
   value=$(echo "$value" + "$diff" | bc)
   str_value=$value"dB"
-  out_f="${2%.*}[$str_value]$suffix"
+  out_f="${1%.*}[$str_value]$suffix"
   echo "parameter: $str_value"
-  output=$(ffmpeg -i "$2" -filter:a "volume=$str_value" -y "$out_f" 2>&1);
+  output=$(ffmpeg -i "$1" -filter:a "volume=$str_value" -y "$out_f" 2>&1);
   file=$out_f
   diff=$(diff_db)
   echo "diff: ${diff}dB"
   echo '------'
   [ "$diff" != "0" ]
 do true; done
+echo "output=$out_f"
 $(printf "$out_f\n" >> ../complete.list)
