81a0de330c14f669872320b9bf8059ccb558f558
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-01 18:16:53 +0800

    Update move_to_output.php

--- move_to_output.php
+++ move_to_output.php
@@ -5,7 +5,7 @@
     $file = explode("\n", $output);
     array_pop($file);
     foreach($file as $f){
-      exec('mv uploads/'.$f.' output/ > /dev/null 2>/dev/null &');
+      exec('[ -f "'.$f.'" ] && mv uploads/'.$f.' output/ > /dev/null 2>/dev/null &');
       echo $f."<br>";
     }
   }
