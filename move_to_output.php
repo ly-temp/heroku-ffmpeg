d29f1f7ed31a84532d113f7ebf2e58d5d3ac8eb2
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-01 18:02:39 +0800

    Update move_to_output.php

--- move_to_output.php
+++ move_to_output.php
@@ -1,10 +1,12 @@
 <?php
   $output=shell_exec("cat complete.list");
-  exec("echo > complete.list");
-  $file = explode("\n", $output);
-  array_pop($file);
-  foreach($file as $f){
-    exec('mv uploads/'.$f.' output/ > /dev/null 2>/dev/null &');
-    echo $f."<br>";
+  if(shell_exec("echo".$output." | wc -w") != 0){
+    exec("echo > complete.list");
+    $file = explode("\n", $output);
+    array_pop($file);
+    foreach($file as $f){
+      exec('mv uploads/'.$f.' output/ > /dev/null 2>/dev/null &');
+      echo $f."<br>";
+    }
   }
 ?>
