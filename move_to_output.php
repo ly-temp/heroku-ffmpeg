6d83f6a81fc9245403febe219f29b4da0b500362
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-01 16:48:44 +0800

    Create move_to_output.php

---
+++ move_to_output.php
@@ -0,0 +1,9 @@
+<?php
+  $output=$("cat complete.list");
+  $file = explode("\n", $output);
+  array_pop($file);
+  foreach($file as $f){
+    exec('mv uploads/'.$f.' output/ > /dev/null 2>/dev/null &');
+    echo $f."<br>";
+  }
+?>
