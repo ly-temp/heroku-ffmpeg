49bbaa1609958950953b0745da36107e89cdc8aa
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-01 17:00:31 +0800

    Update ffmpeg.php

--- ffmpeg.php
+++ ffmpeg.php
@@ -15,17 +15,17 @@ function call_ffmpeg($prefix){
    //for($i = 0; !empty($_POST[$prefix.$i]); $i++){
    $i = 0;
    while(!empty($_POST[$prefix.$i])){
-     echo $i.":".$file_list[$i]."->".$_POST[$prefix.'c'.$i].'<br>';
+     //echo $i.":".$file_list[$i]."->".$_POST[$prefix.'c'.$i].'<br>';
      switch($_POST[$prefix.$i]){
        //case 1://no action
          //break;
        case 2://nokia
-         $suffix = has_video($file_list[$i]) ? ".3gp" : ".mp3";
+         //$suffix = has_video($file_list[$i]) ? ".3gp" : ".mp3";
          //$out_file = shell_exec("../bash/nokia_LY.sh ".$suffix.' "'.$file_list[$i].'" | tail -1');
-         exec("../bash/nokia_LY.sh ".$suffix.' "'.$file_list[$i].'" > /dev/null 2>/dev/null &');
+         exec("../bash/nokia_LY.sh ".$suffix.'" '.$file_list[$i].'" > /dev/null 2>/dev/null &');
          break;
        case 3://audio to db
-         $out_file = shell_exec("../bash/to_db_LY.sh ".$suffix.' "'.$file_list[$i].'" | tail -1');
+         exec("../bash/to_db_LY.sh ".$suffix.'" '.$file_list[$i].'" > /dev/null 2>/dev/null &');
 
          break;
        case 4://change format
