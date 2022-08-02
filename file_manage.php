3ccd84751e81a0e0ae8a27fbc20a4cd136e27154
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-02 17:46:39 +0800

    Update file_manage.php

--- file_manage.php
+++ file_manage.php
@@ -1,44 +1,13 @@
 <?php
-  $dom = new DOMDocument('1.0', 'utf-8');
-
   $output_file = shell_exec("ls output/");
   $output_file = explode("\n", $output_file);
   array_pop($output_file);
+  $folder="/output/";
   foreach($output_file as $file){
-    $anchor = $dom->createElement('a', $file);
-    $anchor->setAttribute('href', $file);
-    link_anchor($anchor, "/output/".$file);
-    $anchor->setAttribute('download',"");    
-    $dom->appendChild($anchor);
-    
-    add_n_space($dom, 2);
-    
-    $anchor = $dom->createElement('a', "preview");
-    link_anchor($anchor, "/output/".$file);
-    $dom->appendChild($anchor);  
-    add_br($dom);
-  }
-
-  echo $dom->saveHTML();
-
-
-  function link_anchor($anchor,$filename){
-    $anchor->setAttribute('href', $filename);
-    $anchor->setAttribute('target', '_blank');    
-  }
-  function add_n_space($dom, $n){
-    for($i = 0; $i < $n; $i++){
-      add_space($dom);
-    }
-  }
-  function add_space($dom){
-    //&amp;nbsp;
-    $template = $dom->createDocumentFragment();
-    $template->appendXML('&#160;');
-    $dom->appendChild($template);
-  }
-  function add_br($dom){
-    $br = $dom->createElement('br');
-    $dom->appendChild($br);    
+    $file_path = $folder.$file;
+    echo '<a href="'.$file_path.'" target="_blank" download>'.$file.'</a>'
+    echo '&#160;';
+    echo '<a href="'.$file_path.'" target="_blank" download>preview</a>'
+    echo '<br>';
   }
 ?>
