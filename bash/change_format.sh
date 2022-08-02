4fd6d8c5c3162734dbd7eb4b612b25761506e203
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-02 17:35:31 +0800

    Update change_format.sh

--- bash/change_format.sh
+++ bash/change_format.sh
@@ -2,5 +2,5 @@
 #input [infile] [format]
 [[ $2 = .* ]] && suffix="$2" || suffix=".""$2"
 out_f="${1%.*}$suffix"
-ffmpeg -i "$1" "$out_f"
+ffmpeg -i "$1" "$out_f" -y
 $(printf "$out_f\n" >> ../complete.list)
