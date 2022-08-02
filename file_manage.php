210d72d98796f68aae4200af39cdea635c9e0b85
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-02 17:47:15 +0800

    Update file_manage.php

--- file_manage.php
+++ file_manage.php
@@ -5,9 +5,9 @@
   $folder="/output/";
   foreach($output_file as $file){
     $file_path = $folder.$file;
-    echo '<a href="'.$file_path.'" target="_blank" download>'.$file.'</a>'
-    echo "&#160;";
-    echo '<a href="'.$file_path.'" target="_blank" download>preview</a>'
+    echo '<a href="'.$file_path.'" target="_blank" download>'.$file.'</a>';
+    echo '&#160;';
+    echo '<a href="'.$file_path.'" target="_blank" download>preview</a>';
     echo '<br>';
   }
 ?>
