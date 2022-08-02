813513e4d14683c7609eac37f9d0b575fc5a38c2
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-02 17:51:36 +0800

    Update file_manage.php

--- file_manage.php
+++ file_manage.php
@@ -1,14 +1,21 @@
 <?php
+  echo '~~~output~~~';
+  echo_folder("output/");
+  echo '~~~uploads~~~';
+  echo_folder("uploads/");
 
-  $folder="output/";
-  $output_file = shell_exec("ls ".$folder);
-  $output_file = explode("\n", $output_file);
-  array_pop($output_file);
-  foreach($output_file as $file){
-    $file_path = '/'.$folder.$file;
-    echo '<a href="'.$file_path.'" target="_blank" download>'.$file.'</a>';
-    echo '&#160;';
-    echo '<a href="'.$file_path.'" target="_blank" download>preview</a>';
-    echo '<br>';
+  function echo_folder($folder){
+    $output_file = shell_exec("ls ".$folder);
+    $output_file = explode("\n", $output_file);
+    array_pop($output_file);
+    foreach($output_file as $file){
+      $file_path = '/'.$folder.$file;
+      echo '<a href="'.$file_path.'" target="_blank" download>'.$file.'</a>';
+      echo '&#160;';
+      echo '<a href="'.$file_path.'" target="_blank" download>preview</a>';
+      echo '<br>';
+    }
   }
 ?>
+<br>
+<a href="/">back</a>
