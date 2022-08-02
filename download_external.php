947a0984b524fb4700a5c34ebad71a1a1630f517
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-02 11:42:39 +0800

    Update download_external.php

--- download_external.php
+++ download_external.php
@@ -7,5 +7,6 @@ meta http-equiv="refresh" content="0; url=/" />
   $output_list = "external.list";
   exec("chmod +x bash/*");
   exec('wget -O '.$output_list.' "'.$_GET['url'].'"');
-  exec('bash/get_external.sh '.$output_list);
+  chdir("downloads/");
+  exec('../bash/get_external.sh '.$output_list);
 ?>
