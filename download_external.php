b89004ec6b5afb78b8c990ad2ffe094eff95feb6
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-02 11:50:15 +0800

    Update download_external.php

--- download_external.php
+++ download_external.php
@@ -4,9 +4,12 @@ meta http-equiv="refresh" content="0; url=/" />
 <a href="/">manual redirect</a>
 
 <?php
+  $target_dir = "uploads/";
   $output_list = "external.list";
+
+  exec("mkdir -p ".$target_dir);
   exec("chmod +x bash/*");
   exec('wget -O '.$output_list.' "'.$_GET['url'].'"');
-  chdir("uploads/");
+  chdir($target_dir);
   exec('wget -i '.$output_list.' >/dev/null 2>/dev/null &');
 ?>
