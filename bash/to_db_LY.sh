b68e7c15c7451ad745deacc4784e9912da27253c
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-02 17:10:45 +0800

    Update to_db_LY.sh

--- bash/to_db_LY.sh
+++ bash/to_db_LY.sh
@@ -31,10 +31,10 @@ while
   echo "parameter: $str_value"
   output=$(ffmpeg -i "$1" -filter:a "volume=$str_value" -y "$out_f" 2>&1)
   
-  #fix 3gp cannot select codec
-  #if [ $(grep -E "(Default encoder for format 3gp \(codec amr_nb\) is probably disabled.|Conversion failed!)" <<< "$output" | wc -w) -gt 0 ]; then
-  #	output=$(ffmpeg -i "$1" -filter:a "volume=$str_value" -vcodec libx264 -acodec aac -y "$out_f" 2>&1)
-  #fi
+  fix 3gp cannot select codec
+  if [ $(grep -E "(Default encoder for format 3gp \(codec amr_nb\) is probably disabled.|Conversion failed!)" <<< "$output" | wc -w) -gt 0 ]; then
+  	output=$(ffmpeg -i "$1" -filter:a "volume=$str_value" -vcodec libx264 -acodec aac -y "$out_f" 2>&1)
+  fi
   
   file="$out_f"
   diff=$(diff_db)
