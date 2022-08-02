0ce541a80c2adf71ff9ba246ec61052e67d4d037
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-02 14:18:24 +0800

    Update to_db_LY.sh

--- bash/to_db_LY.sh
+++ bash/to_db_LY.sh
@@ -11,8 +11,6 @@ fi
 diff_db(){
 	current_db=($(ffmpeg -i "$file" -filter:a volumedetect -f null /dev/null 2>&1 | grep "mean_volume:" | grep -o ":.*" | cut -d' ' -f2))
 	diff=$(echo "$target_db - $current_db" | bc)
-	echo "current_db $current_db" >> log.txt #testing
-	echo "diff $diff" >> log.txt
 	echo "$diff"
 }
 
@@ -28,24 +26,21 @@ while
   fi
 
   value=$(echo "$value" + "$diff" | bc)
-  echo "value $value" >> log.txt	#testing
   str_value=$value"dB"
   out_f="${1%.*}[$str_value]$suffix"
-  echo "parameter: $str_value" >> log.txt
+  echo "parameter: $str_value"
   output=$(ffmpeg -i "$1" -filter:a "volume=$str_value" -y "$out_f" 2>&1)
   
   #fix 3gp cannot select codec
-  #if [ $(grep "Default encoder for format 3gp (codec amr_nb) is probably disabled." <<< "$output" | wc -w) -gt 0 ]; then
+  #if [ $(grep -E "(Default encoder for format 3gp \(codec amr_nb\) is probably disabled.|Conversion failed!)" <<< "$output" | wc -w) -gt 0 ]; then
   #	output=$(ffmpeg -i "$1" -filter:a "volume=$str_value" -vcodec libx264 -acodec aac -y "$out_f" 2>&1)
   #fi
   
-  echo "$output" >> log.txt #testing
-  
+  echo "$output"
   file="$out_f"
   diff=$(diff_db)
   echo "diff: ${diff}dB"
   echo '------'
   [ "$diff" != "0" ]
 do true; done
-echo "$out_f"
 $(printf "$out_f\n" >> ../complete.list)
