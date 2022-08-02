d8a4574254ddbf6c5660ae409ba0ec02bacf3ed0
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-02 17:48:51 +0800

    Update file_manage.php

--- file_manage.php
+++ file_manage.php
@@ -1,10 +1,11 @@
 <?php
-  $output_file = shell_exec("ls output/");
+
+  $folder="output/";
+  $output_file = shell_exec("ls ".$folder);
   $output_file = explode("\n", $output_file);
   array_pop($output_file);
-  $folder="/output/";
   foreach($output_file as $file){
-    $file_path = $folder.$file;
+    $file_path = '/'.$folder.$file;
     echo '<a href="'.$file_path.'" target="_blank" download>'.$file.'</a>';
     echo '&#160;';
     echo '<a href="'.$file_path.'" target="_blank" download>preview</a>';
