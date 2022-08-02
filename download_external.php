0e9e1e4b80ebdcb25be8a21f4aa81066a0fc45ff
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-02 11:00:57 +0800

    Create download_external.php

---
+++ download_external.php
@@ -0,0 +1,11 @@
+<head>
+meta http-equiv="refresh" content="0; url=/" />
+</head>
+<a href="/">manual redirect</a>
+
+<?php
+  $output_list = "external.list";
+  exec("chmod +x bash/*");
+  exec('wget -O '.$output_list.' "'.$_GET['url'].'"');
+  exec('bash/get_external.sh '.$output_list);
+?>
