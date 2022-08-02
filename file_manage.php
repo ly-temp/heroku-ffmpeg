97a3a31d42a05959dca6419621c922fe25e0ec85
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-02 18:03:13 +0800

    Update file_manage.php

--- file_manage.php
+++ file_manage.php
@@ -4,8 +4,8 @@
   echo '~~~uploads~~~<br>';
   echo_folder("uploads/");
 
-  function echo_folder($folder){
-    $output_file = shell_exec("ls ".$folder);
+  function echo_folder($folder){ 
+    $output_file = shell_exec("ls ".$folder.' | tee '.substr($folder, 0, -1).'.list');
     $output_file = explode("\n", $output_file);
     array_pop($output_file);
     foreach($output_file as $file){
