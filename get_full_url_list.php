1074011354166bd0919899f1e10441352f0a71ac
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-03 10:50:29 +0800

    Update get_full_url_list.php

--- get_full_url_list.php
+++ get_full_url_list.php
@@ -1,6 +1,6 @@
 <?php
   if(isset($_GET['type'])){
-    $full_url = 'https://'.$_SERVER['SERVER_NAME'].$_GET['type'].'/';
+    $full_url = 'https://'.$_SERVER['SERVER_NAME'].'/'.$_GET['type'].'/';
     $list_file = $_GET['type'].'.list';
     exec('sed -i "s|^|'.$full_url.'|g" '.$list_file);
     echo '<head><meta http-equiv="refresh" content="0; url=/'.$list_file.'" /></head>';
