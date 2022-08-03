81e68901733eac11ed01ab244b93a63172ef46de
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-03 10:27:36 +0800

    Create get_full_url_list.php

---
+++ get_full_url_list.php
@@ -0,0 +1,10 @@
+<?php
+  if(isset($_GET['type'])){
+    $full_url = 'https://'.$_SERVER['SERVER_NAME'].$_GET['type'].'/';
+    $list_file = $_GET['type']).'.list';
+    exec('sed -i "s|^|'.$full_url.'|g" '.$list_file);
+  }else{
+    echo "invalid dir as 'type'";
+  }
+
+?>
