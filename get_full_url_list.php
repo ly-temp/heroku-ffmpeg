40ad3162a6650853b1f62203144ab375bdae20a6
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-03 10:43:56 +0800

    Update get_full_url_list.php

--- get_full_url_list.php
+++ get_full_url_list.php
@@ -3,6 +3,7 @@
     $full_url = 'https://'.$_SERVER['SERVER_NAME'].$_GET['type'].'/';
     $list_file = $_GET['type'].'.list';
     exec('sed -i "s|^|'.$full_url.'|g" '.$list_file);
+    echo '<head><meta http-equiv="refresh" content="0; url=/'.$list_file.'" /></head>';
     echo '<a href="/'.$list_file.'">manual redirect</a>';
   }else{
     echo "invalid dir as 'type'";
