ff1e4e52dcccfae1bd4b3831d311787c4a096ecd
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-03 10:35:19 +0800

    Update get_full_url_list.php

--- get_full_url_list.php
+++ get_full_url_list.php
@@ -1,7 +1,7 @@
 <?php
   if(isset($_GET['type'])){
     $full_url = 'https://'.$_SERVER['SERVER_NAME'].$_GET['type'].'/';
-    $list_file = $_GET['type']).'.list';
+    $list_file = $_GET['type'].'.list';
     exec('sed -i "s|^|'.$full_url.'|g" '.$list_file);
     echo '<a href="/'.$list_file.'">manual redirect</a>';
   }else{
