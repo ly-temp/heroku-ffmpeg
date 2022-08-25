b97780e9bf765859983fbac87469c91455c70cfe
ly-temp <100673133+ly-temp@users.noreply.github.com>
2022-08-25 17:55:35 +0800

    Update download_external.php

--- download_external.php
+++ download_external.php
@@ -7,9 +7,10 @@
   $target_dir = "uploads/";
   $output_list = "external.list";
 
+  $url = substr($_SERVER['REQUEST_URI'], strpos($_SERVER['REQUEST_URI'], "=")+1);
   exec("mkdir -p ".$target_dir);
   exec("chmod +x bash/*");
-  exec('wget -O '.$output_list.' "'.$_GET['url'].'"');
+  exec('wget -O '.$output_list.' "'.$url.'"');
   chdir($target_dir);
   exec('wget -i ../'.$output_list.' >/dev/null 2>/dev/null &');
 ?>
