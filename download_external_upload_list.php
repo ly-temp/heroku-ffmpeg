57517b534b6b68d58658ee29eed46ed1d7510998
ly-temp <100673133+ly-temp@users.noreply.github.com>
2022-08-28 07:48:03 +0800

    Update download_external_upload_list.php

--- download_external_upload_list.php
+++ download_external_upload_list.php
@@ -1,8 +1,9 @@
 <?php
-if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)) {
-    echo "fail";
-}else{
-    echo "success";
-}
-
+  if (move_uploaded_file($_FILES['file']['tmp_name'], "./external.list")) {
+      echo "fail";
+  }else{
+      echo "success";
+      exec("chmod +x bash/*");
+      exec("bash/download_external.sh");      
+  }
 ?>
