84da436798a23e1bd4c3840df5b90f273f4bd68b
ly-temp <100673133+ly-temp@users.noreply.github.com>
2022-08-28 07:49:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -18,6 +18,12 @@
 <br>
 <a href="download_external.php">download_external.php?url=</a>
 <br>
+upload list:
+<form action="download_external_upload_list.php " method="post" enctype="multipart/form-data">
+  <input name="upload" type="file" multiple />
+  <input type="submit" value="Send files" />
+</form>
+<br>
 <a href="/cmd.php">cmd</a>
 </body>
 </html>
