4ce9c5d2f7e0b52c20bf7b79391072ad47c49610
ly-temp <100673133+ly-temp@users.noreply.github.com>
2022-08-28 07:45:24 +0800

    Delete download_external_start.php

--- download_external_start.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $target_dir = "uploads/";
-  $output_list = "external.list";
-
-  exec("mkdir -p ".$target_dir);
-  exec("chmod +x bash/*");
-  chdir($target_dir);
-  exec('wget -i ../'.$output_list.' >/dev/null 2>/dev/null &');
-?>
