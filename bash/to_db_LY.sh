736e24f8ce08e8950ed4596935b81de1eeae59ac
ly-temp <100673133+ly-temp@users.noreply.github.com>
2022-09-10 13:13:00 +0800

    Add files via upload

--- bash/to_db_LY.sh
+++ bash/to_db_LY.sh
@@ -8,36 +8,6 @@ else
 	suffix=$3
 fi
 
-#my_bc
-: '
-	#pipline input: a -+ b
-	#bash bc for simple add / subtraction
-
-	function break_float(){
-		if grep -q '\.' <<< "$1";then
-			sed -e "s/\./ /g" <<< "$1"
-		else
-			echo "$1 0"
-		fi
-	}
-
-	function my_bc(){
-		read string
-		sign=$(cut -d' ' -f2 <<< "$string")
-		a=$(cut -d' ' -f1 <<< "$string")
-		b=$(cut -d' ' -f3 <<< "$string")
-		read -r a_int a_float <<< $(break_float "$a")
-		read -r b_int b_float <<< $(break_float "$b")
-		#echo "$a_int	$b_int"
-		#echo "$a_float	$b_float"
-
-		[ "$sign" = "-" ] && b_int="-$b_int" && b_float="-$b_float"
-
-		echo "$(($a_int+$b_int)).$(($a_float+$b_float))"
-	}
-'
-#end of my_bc
-
 diff_db(){
 	current_db=($(ffmpeg -i "$file" -filter:a volumedetect -f null /dev/null 2>&1 | grep "mean_volume:" | grep -o ":.*" | cut -d' ' -f2))
 	diff=$(echo "$target_db - $current_db" | bc)
@@ -55,7 +25,7 @@ while
 	rm "$out_f"
   fi
 
-  value=$(echo "$value" + "$diff" | bc)
+  value=$(echo "$value + $diff" | bc)
   str_value="$value""dB"
   out_f="${1%.*}[$str_value]$suffix"
   echo "parameter: $str_value"
